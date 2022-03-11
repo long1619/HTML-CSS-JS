8a31b96f5a3342705514f7c625728a435a6880fa
long1619 <duclong1619@gmail.com>
2022-03-11 22:04:57 +0700

    weather

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const APP_ID = `40ee978929d4d63f0f0a10752664d887`;
 /* */
 const DEFAULT_VALUE = "---";
+const searchInput = document.querySelector("#search-input");
 const cityName = document.querySelector(".city-name");
 const weatherState = document.querySelector(".weather-state");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -11,7 +12,7 @@ const sunset = document.querySelector(".sunset");
 const humidity = document.querySelector(".humidity");
 const windspeed = document.querySelector(".wind-speed");
 /* */
-const searchInput = document.querySelector("#search-input");
+
 searchInput.addEventListener("change", function (e) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`
@@ -35,36 +36,38 @@ searchInput.addEventListener("change", function (e) {
     windspeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
   });
 });
-/*-------------Trợ Lý Ảo------------- */
+
+/*-------------------------Trợ Lý Ảo---------------------------- */
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 const synth = window.speechSynthesis; // giọng nói
-const microphone = document.querySelector(".microphone");
+
 // config giọng nói tiếng việt
 recognition.lang = "vi-VI";
 // kết quả trả về ngay khi tìm kiếm bằng giọng nói
 recognition.continuous = false;
-//-------------//
+//------giọng nói-------//
 const speak = (text) => {
   if (synth.speaking) {
-    console.error("busy", "speaking....");
+    console.error("Busy, Speaking...");
     return;
   }
   const utter = new SpeechSynthesisUtterance(text);
   utter.onend = () => {
-    console.log("SpeechSynthesisUtterance.onend");
+    console.log("speechSynthesisUtteranceInstance.onend");
   };
   utter.onerror = (err) => {
-    console.log("SpeechSynthesisUtterance.onerror", err);
+    console.error("SpeechSynthesisUtterance.onerror", err);
   };
   synth.speak(utter);
 };
-//--------------------------------------------//
+/*event microphone */
+const microphone = document.querySelector(".microphone");
 const handleVoice = (text) => {
   console.log("text", text);
-
   // vd: thời tiết tại hà nội =>["thời tiết tại","hà nội"]
   const handledText = text.toLowerCase();
+  /*------------------------------------------ */
   if (handledText.includes("thời tiết tại")) {
     const location = handledText.split("tại")[1].trim();
     console.log("location", location);
@@ -74,25 +77,27 @@ const handleVoice = (text) => {
     return;
   }
   /*thay đổi màu nền */
+  const container = document.querySelector(".container");
   if (handledText.includes("thay đổi màu nền")) {
     const color = handledText.split("màu nền")[1].trim();
-    const container = document.querySelector(".container");
-    container.style.backgroundColor = color;
+    container.style.background = color;
     return;
   }
   /*reset màu */
   if (handledText.includes("màu nền mặc định")) {
-    container.style.backgroundColor = "";
+    container.style.background = "";
     return;
   }
   /*thời gian hiện tại  */
   if (handledText.includes("mấy giờ")) {
     const textToSpeech = `${moment().hours()} hours ${moment().minutes()} minutes`;
+    speak(textToSpeech);
+    return;
   }
   speak("Try again"); // khi k có các trường hợp trên
 };
-//--------------------------------------------//
-/*event microphone */
+
+//--------------------------------------------------------------//
 microphone.addEventListener("click", (e) => {
   e.preventDefault();
   recognition.start(); // hỏi truy cập microphone
@@ -106,11 +111,12 @@ recognition.onspeechend = () => {
 /*khi có lỗi xảy ra */
 recognition.onerror = (err) => {
   console.error(err);
-  microphone.classList.remove("recording");
+  microphone.classList.remove("recording"); // khi kết thúc
 };
 /*khi có kết quả trả về */
 recognition.onresult = (e) => {
   console.log("onresult", e);
-  const text = e.result[0][0].transcript;
+  // dữ liệu trả về
+  const text = e.results[0][0].transcript;
   handleVoice(text);
 };
